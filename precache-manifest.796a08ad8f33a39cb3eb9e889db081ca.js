50afa9efe234eb60b0fab0f56bd59e3aec1cce73
Mihir Mehta <mmihir1998@gmail.com>
2020-05-28 02:41:58 +0530

    Updates

---
+++ precache-manifest.796a08ad8f33a39cb3eb9e889db081ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5c26be3335a521764a1bd34f62b7f94",
+    "url": "/React_PortFolio/index.html"
+  },
+  {
+    "revision": "d987ace38484c45499d9",
+    "url": "/React_PortFolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "86edfa7eeab9f265c299",
+    "url": "/React_PortFolio/static/css/main.73411453.chunk.css"
+  },
+  {
+    "revision": "d987ace38484c45499d9",
+    "url": "/React_PortFolio/static/js/2.5377425d.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/React_PortFolio/static/js/2.5377425d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86edfa7eeab9f265c299",
+    "url": "/React_PortFolio/static/js/main.86e0c8be.chunk.js"
+  },
+  {
+    "revision": "e1e2b01746796ddf79a7",
+    "url": "/React_PortFolio/static/js/runtime-main.75cd922d.js"
+  }
+]);
